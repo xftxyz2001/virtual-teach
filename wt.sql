3139258baf301ff138bc940a0ec7c6e5f0715a55
通晓宇宙 <2581011320@qq.com>
2024-05-13 14:34:17 +0800

    sql

--- wt.sql
+++ wt.sql
@@ -1,92 +1,192 @@
-CREATE TABLE IF NOT EXISTS `user` (
-    `id` BIGINT PRIMARY KEY COMMENT '用户ID',
-    `login` VARCHAR(255) COMMENT '用户名',
-    `password` VARCHAR(255) COMMENT '密码',
-    `nick_name` VARCHAR(255) COMMENT '用户昵称',
-    `telephone` VARCHAR(255) COMMENT '电话号码',
-    `teacher_name` VARCHAR(255) COMMENT '教师名',
-    `personnel_signature` VARCHAR(255) COMMENT '个人签名',
-    `department_id` BIGINT COMMENT '部门ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '用户表';
+/*
+ Navicat Premium Data Transfer
 
-CREATE TABLE IF NOT EXISTS `department` (
-    `id` BIGINT PRIMARY KEY COMMENT '部门ID',
-    `name` VARCHAR(255) COMMENT '名称',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '部门表';
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80200 (8.2.0)
+ Source Host           : localhost:3306
+ Source Schema         : wt
 
-CREATE TABLE IF NOT EXISTS `teach_and_research_section` (
-    `id` BIGINT PRIMARY KEY COMMENT '教研室ID',
-    `name` VARCHAR(255) COMMENT '名称',
-    `group_leader` BIGINT COMMENT '组长用户ID',
-    `course_id` BIGINT COMMENT '课程ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '教研室表';
+ Target Server Type    : MySQL
+ Target Server Version : 80200 (8.2.0)
+ File Encoding         : 65001
 
-CREATE TABLE IF NOT EXISTS `section_user` (
-    `id` BIGINT PRIMARY KEY COMMENT 'ID',
-    `section_id` BIGINT COMMENT '教研室ID',
-    `user_id` BIGINT COMMENT '用户ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '教研室用户关联表';
+ Date: 13/05/2024 14:33:59
+*/
 
-CREATE TABLE IF NOT EXISTS `course` (
-    `id` BIGINT PRIMARY KEY COMMENT '课程ID',
-    `name` VARCHAR(255) COMMENT '课程名称',
-    `introduction` VARCHAR(255) COMMENT '课程介绍',
-    `department_id` BIGINT COMMENT '部门ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '课程表';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE IF NOT EXISTS `teacher_course` (
-    `id` BIGINT PRIMARY KEY COMMENT 'ID',
-    `user_id` BIGINT COMMENT '用户ID',
-    `course_id` BIGINT COMMENT '课程ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '教师课程关联表';
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` bigint NOT NULL COMMENT '课程ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '课程名称',
+  `introduction` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '课程介绍',
+  `department_id` bigint NULL DEFAULT NULL COMMENT '部门ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '课程表' ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS `meeting` (
-    `id` BIGINT PRIMARY KEY COMMENT '会议ID',
-    `invite_num` VARCHAR(255) COMMENT '邀请码',
-    `topic` VARCHAR(255) COMMENT '会议主题',
-    `user_id` BIGINT COMMENT '用户ID',
-    `start_time` TIMESTAMP COMMENT '开始时间',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '学术会议表';
+-- ----------------------------
+-- Records of course
+-- ----------------------------
 
-CREATE TABLE IF NOT EXISTS `news` (
-    `id` BIGINT PRIMARY KEY COMMENT '新闻ID',
-    `topic` VARCHAR(255) COMMENT '主题',
-    `content` VARCHAR(255) COMMENT '内容',
-    -- `date` VARCHAR(255) COMMENT '日期',
-    `publisher` BIGINT COMMENT '发布教研室ID',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '新闻公告表';
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `id` bigint NOT NULL COMMENT '部门ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '部门表' ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS `system_notification` (
-    `id` BIGINT PRIMARY KEY COMMENT '系统消息ID',
-    `topic` VARCHAR(255) COMMENT '主题',
-    `notification_type` VARCHAR(255) COMMENT '通知类型',
-    `content` VARCHAR(255) COMMENT '内容',
-    -- `date` VARCHAR(255) COMMENT '日期',
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted` TINYINT(1) DEFAULT 0 COMMENT '是否删除'
-) COMMENT = '系统消息表';
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for meeting
+-- ----------------------------
+DROP TABLE IF EXISTS `meeting`;
+CREATE TABLE `meeting`  (
+  `id` bigint NOT NULL COMMENT '会议ID',
+  `invite_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邀请码',
+  `topic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '会议主题',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '用户ID',
+  `start_time` timestamp NULL DEFAULT NULL COMMENT '开始时间',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学术会议表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of meeting
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for news
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news`  (
+  `id` bigint NOT NULL COMMENT '新闻ID',
+  `topic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '主题',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '内容',
+  `publisher` bigint NULL DEFAULT NULL COMMENT '发布教研室ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '新闻公告表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of news
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for section_user
+-- ----------------------------
+DROP TABLE IF EXISTS `section_user`;
+CREATE TABLE `section_user`  (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `section_id` bigint NULL DEFAULT NULL COMMENT '教研室ID',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '用户ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '教研室用户关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of section_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for system_notification
+-- ----------------------------
+DROP TABLE IF EXISTS `system_notification`;
+CREATE TABLE `system_notification`  (
+  `id` bigint NOT NULL COMMENT '系统消息ID',
+  `topic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '主题',
+  `notification_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '通知类型',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '内容',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '系统消息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of system_notification
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for teach_and_research_section
+-- ----------------------------
+DROP TABLE IF EXISTS `teach_and_research_section`;
+CREATE TABLE `teach_and_research_section`  (
+  `id` bigint NOT NULL COMMENT '教研室ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
+  `group_leader` bigint NULL DEFAULT NULL COMMENT '组长用户ID',
+  `course_id` bigint NULL DEFAULT NULL COMMENT '课程ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '教研室表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teach_and_research_section
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for teacher_course
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_course`;
+CREATE TABLE `teacher_course`  (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '用户ID',
+  `course_id` bigint NULL DEFAULT NULL COMMENT '课程ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '教师课程关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher_course
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL COMMENT '用户ID',
+  `login` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
+  `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
+  `telephone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话号码',
+  `teacher_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '教师名',
+  `personnel_signature` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '个人签名',
+  `department_id` bigint NULL DEFAULT NULL COMMENT '部门ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;

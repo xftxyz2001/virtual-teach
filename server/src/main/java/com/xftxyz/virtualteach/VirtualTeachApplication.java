04c515e76171be2944cac8c86af389afbab30f5f
通晓宇宙 <2581011320@qq.com>
2024-05-13 10:08:19 +0800

    init

---
+++ server/src/main/java/com/xftxyz/virtualteach/VirtualTeachApplication.java
@@ -0,0 +1,13 @@
+package com.xftxyz.virtualteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualTeachApplication.class, args);
+    }
+
+}

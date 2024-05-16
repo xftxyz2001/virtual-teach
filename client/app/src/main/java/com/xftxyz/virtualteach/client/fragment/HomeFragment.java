897ced4ab130662d50be51f93b2e22f446070ba9
通晓宇宙 <2581011320@qq.com>
2024-05-16 10:09:56 +0800

    MainActivity 按钮绑定

--- client/app/src/main/java/com/xftxyz/virtualteach/client/fragment/HomeFragment.java
+++ client/app/src/main/java/com/xftxyz/virtualteach/client/fragment/HomeFragment.java
@@ -11,9 +11,7 @@ import androidx.fragment.app.Fragment;
 import com.xftxyz.virtualteach.client.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * 首页
  */
 public class HomeFragment extends Fragment {
 
@@ -30,9 +28,37 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        initWidgets(view);
-        return view;
+        View inflate = inflater.inflate(R.layout.fragment_home, container, false);
+        initWidgets(inflate);
+        initListeners();
+        return inflate;
+    }
+
+    private void initListeners() {
+        btnToNews.setOnClickListener(v -> {
+            // 前往新闻公告
+        });
+        btnToSection.setOnClickListener(v -> {
+            // 前往虚拟教研室
+        });
+        btnToMeeting.setOnClickListener(v -> {
+            // 前往会议提醒
+        });
+        btnToNotification.setOnClickListener(v -> {
+            // 前往系统公告
+        });
+        btnToProfile.setOnClickListener(v -> {
+            // 前往个人信息
+        });
+        btnToMyCourse.setOnClickListener(v -> {
+            // 前往我的课程
+        });
+        btnToMyPost.setOnClickListener(v -> {
+            // 前往我的发布
+        });
+        btnToSetting.setOnClickListener(v -> {
+            // 前往设置
+        });
     }
 
     private void initWidgets(View view) {
--- client/app/src/main/java/com/xftxyz/virtualteach/client/fragment/MineFragment.java
+++ client/app/src/main/java/com/xftxyz/virtualteach/client/fragment/MineFragment.java
@@ -1,66 +1,54 @@
 package com.xftxyz.virtualteach.client.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 import com.xftxyz.virtualteach.client.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link MineFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * 我的
  */
 public class MineFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MineFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MineFragment newInstance(String param1, String param2) {
-        MineFragment fragment = new MineFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Button btnToProfile;
+    private Button btnToMyCourse;
+    private Button btnToMyPost;
+    private Button btnToSetting;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_mine, container, false);
+        initWidgets(inflate);
+        initListeners();
+        return inflate;
+    }
+
+    private void initListeners() {
+        btnToProfile.setOnClickListener(v -> {
+            // 前往个人信息
+        });
+        btnToMyCourse.setOnClickListener(v -> {
+            // 前往我的课程
+        });
+        btnToMyPost.setOnClickListener(v -> {
+            // 前往我的发布
+        });
+        btnToSetting.setOnClickListener(v -> {
+            // 前往设置
+        });
+    }
+
+    private void initWidgets(View view) {
+        btnToProfile = view.findViewById(R.id.btn_mine_to_profile);
+        btnToMyCourse = view.findViewById(R.id.btn_mine_to_my_course);
+        btnToMyPost = view.findViewById(R.id.btn_mine_to_my_post);
+        btnToSetting = view.findViewById(R.id.btn_mine_to_setting);
     }
 }
